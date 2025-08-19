272ffdd090b621ef330becd4805b45a38d35fcf6
Ricardo Verdugo <raverdugo@GenBioinfo10.OCC.med.uchile.cl>
2025-08-19 16:26:51 -0400

    Borrar unidades 3-9

--- Unidad7/DE_tutorial/install_packages.R
+++
@@ -1,8 +0,0 @@
-source("Rfxs.R")
-load.package("preprocessCore", bioC=T)
-load.package("maanova", bioC=T)
-load.package("limma", bioC=T)
-load.package("topGO", bioC=T)
-load.package("org.Mm.eg.db", bioC=T)
-
-
--- Unidad8/Metabarcoding/README.md
+++
@@ -1,1 +0,0 @@
-# Metabarcoding_2021
